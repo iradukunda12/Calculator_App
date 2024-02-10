82c10a2e8ed5dbe453e53bc4410514aacc562a81
kevin Iradukunda <iradukundakvn8@gmail.com>
2024-02-10 17:55:17 +0200

    draweris done

--- lib/Screens/about.dart
+++ lib/Screens/about.dart
@@ -1,19 +1,16 @@
-import 'package:calculate/Screens/about.dart';
+import 'package:calculate/Screens/home_screen/homePage.dart';
 import 'package:calculate/Screens/home_screen/homescreen.dart';
 import 'package:calculate/utils/constants.dart';
 import 'package:flutter/material.dart';
-import 'package:google_nav_bar/google_nav_bar.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+class AboutMe extends StatefulWidget {
+  const AboutMe({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<AboutMe> createState() => _AboutMeState();
 }
 
-class _HomePageState extends State<HomePage> {
-  int _activepos = 0;
-
+class _AboutMeState extends State<AboutMe> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,7 +20,7 @@ class _HomePageState extends State<HomePage> {
         title: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 60.0),
           child: Text(
-            "HomePage",
+            "About ME",
             style: TextStyle(
               fontWeight: FontWeight.bold,
               color: kPrimaryColor,
@@ -108,78 +105,12 @@ class _HomePageState extends State<HomePage> {
                   Navigator.push(
                     context,
                     MaterialPageRoute(
-                      builder: (context) => AboutMe(),
+                      builder: (context) => HomePage(),
                     ),
                   );
                 },
               ),
             ],
-          ),
-        ),
-      ),
-      body: const Center(
-        child: Text(
-          "Hello world!!",
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 20,
-          ),
-        ),
-      ),
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 15.0,
-          vertical: 20.0,
-        ),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(28.0),
-          child: GNav(
-            backgroundColor: kSecondaryColor,
-            color: kPrimaryColor,
-            activeColor: kPrimaryColor,
-            tabBackgroundColor: Colors.grey.shade800,
-            gap: 8,
-            padding: EdgeInsets.all(16.0),
-            tabs: const [
-              GButton(
-                icon: Icons.home,
-                iconSize: 30,
-                text: 'Home',
-              ),
-              GButton(
-                icon: Icons.calculate,
-                iconSize: 30,
-                text: "Calculator",
-              ),
-              GButton(
-                icon: Icons.person,
-                iconSize: 30,
-                text: "Person",
-              ),
-            ],
-            selectedIndex: _activepos,
-            onTabChange: (index) {
-              setState(() {
-                _activepos = index; // Update _activepos with the selected index
-                if (_activepos == 0) {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => HomeScreen(),
-                    ),
-                  );
-                } else if (_activepos == 1) {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => HomePage(),
-                    ),
-                  );
-                } else {
-                  print("hello ....");
-                }
-              });
-            },
           ),
         ),
       ),
